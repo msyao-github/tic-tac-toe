496f7f38ae89bdb961b173b0d816bb4011499919
msyao-github <msyaois@gmail.com>
2016-01-31 21:23:05 -0500

    Edited/added to JS worked with other files

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,9 +5,96 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+//^this is not defined!!! needs to be defined!
 
 // load sass manifest
 require('../styles/index.scss');
+
+function startGame() {
+  for (var i = 1; i <= 9; i = i + 1) {
+    clearBox(i);
+  }
+  document.turn = "X";
+  if (Math.random() < 0.5) {
+    document.turn = "O";
+  }
+  document.winner = null;
+  setMessage("Player "+document.turn + " gets to start.");
+}
+
+function setMessage(msg) {
+  document.getElementById("message").innerText = msg;
+}
+//turns the document id "message" into messages defined by functions below
+
+function nextMove(square) {
+  if (document.winner !== null) {
+    setMessage("Player "+ document.winner + " has won. Restart the game!");
+  } else if (square.innerText == "") {
+    //if square is empty, let player move
+    square.innerText = document.turn;
+    switchTurn();
+  } else {
+    setMessage("That square is occupied.");
+  }
+}
+
+function switchTurn() {
+
+  if (checkForWinner(document.turn)) {
+    setMessage("Woohoo, Player " + document.turn + "!  You win!");
+    document.winner = document.turn;
+  } else if (checkForTie ()) {
+    setMessage("It's a TIE! How about another game?");
+  } else if (document.turn == "X") {
+    document.turn = "O";
+    setMessage("Player " + document.turn + "'s turn!");
+  } else {
+    document.turn = "X";
+    setMessage("Player " + document.turn + "'s turn!");
+  }
+}
+
+function checkForWinner(move) {
+  var result = false;
+  if (checkRow(1, 2, 3, move) ||
+    checkRow(4, 5, 6, move) ||
+    checkRow(7, 8, 9, move) ||
+    checkRow(1, 4, 7, move) ||
+    checkRow(2, 5, 8, move) ||
+    checkRow(3, 6, 9, move) ||
+    checkRow(1, 5, 9, move) ||
+    checkRow(3, 5, 7, move)) {
+//3 in a row, horizontal, vertical and diagonal
+    result = true;
+  }
+  return result;
+}
+
+function checkForTie(){
+  for(var i=1;i<10;i++){
+    if(getBox(i)=="")
+    return false;
+    }
+    return true;
+}
+
+
+function checkRow(a, b, c, move) {
+  var result = false;
+  if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
+    result = true;
+  } //check to see if squares have 3 in a row
+  return result;
+}
+//reference the s1-9 boxes
+function getBox(number) {
+  return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number) {
+  document.getElementById("s" + number).innerText = "";
+}
 
 $(document).ready(() => {
   console.log('It works.');
