5e22590d1d9742945b80e5879a30cbed2f20a8fd
msyao-github <msyaois@gmail.com>
2016-02-04 08:35:00 -0500

    Edited js Wed 2/3

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,98 +9,88 @@ require('./example');
 
 // load sass manifest
 require('../styles/index.scss');
+let winner;
+let player = 'x';
+let xWins = 0;
+let oWins = 0;
+let ties = 0;
 
+let clearBoard = function() {
+  $('.box').text('');
+};
 
+$('.restart-button').on('click', clearBoard());
 
-function startGame() {
-  for (var i = 1; i < 10; i++) {
-    clearBox(i);
-  }
-  document.turn = "X"; //game always starts with X, 0 has 50% chance of next turn
-  if (Math.random() < 0.5) {
-    document.turn = "O";
-  }
-  document.winner = null;
-  setMessage("Player "+document.turn + " gets to start.");
-} //if no previous winner still start game.
-
-function setMessage(msg) {
-  document.getElementById("message").textContent = msg;
-}
-
-
-//turns "message" into messages defined by functions below
-
-//if all boxes occupied and there's a winner, 1st message
-function nextMove(square) {
-  if (document.winner !== null) {
-    setMessage("Player "+ document.winner + " has won. Restart the game!");
-  } else if (square.textContent == "") {
-    //if square is empty, let player move
-    square.textContent = document.turn;
-    switchTurn();
+let score = function() {
+  if (winner === 'x') {
+    xWins++;
+    $('#p1').append(xWins);
+  } else if (winner === 'o') {
+    oWins++;
+    $('#p2').append(oWins);
   } else {
-    setMessage("That square is occupied.");
+    ties++;
+    $('#ties').append(xWins);
   }
-}
 
-function switchTurn() {
-  if (checkForWinner(document.turn)) {
-    setMessage("Woohoo, Player " + document.turn + "!  You win!");
-    document.winner = document.turn;
-  } else if (checkForTie ()) {
-    setMessage("It's a TIE! How about another game?");
-  } else if (document.turn == "X") {
-    document.turn = "O";
-    setMessage("Player " + document.turn + "'s turn!");
-  } else {
-    document.turn = "X";
-    setMessage("Player " + document.turn + "'s turn!");
-  }
-}
+};
 
+let $BoxId = function(num) {
+  return $('#' + num);
+};
 
-function checkForWinner(move) {
-  let result = false;
-  if (checkRow(1, 2, 3, move) ||
-    checkRow(4, 5, 6, move) ||
-    checkRow(7, 8, 9, move) ||
-    checkRow(1, 4, 7, move) ||
-    checkRow(2, 5, 8, move) ||
-    checkRow(3, 6, 9, move) ||
-    checkRow(1, 5, 9, move) ||
-    checkRow(3, 5, 7, move)) {
-//3 in a row, horizontal, vertical and diagonal
-    result = true;
-  }
-  return result;
-}
-
-function checkForTie(){
-  for(var i=1;i<10;i++){
-    if(getBox(i)=="")
-    return false;
-    }
+let checkWinCombo = function(a, b, c) {
+  if ($BoxId(a).text() === player && $BoxId(b).text() === player && $BoxId(c).text() === player) {
     return true;
-}
+  } else {
+    return false;
+  }
+};
 
+let checkWin = function(event) {
+  if(checkWinCombo(1, 2, 3) ||
+    checkWinCombo(4, 5, 6) ||
+    checkWinCombo(7, 8, 9) ||
+    checkWinCombo(1, 4, 7) ||
+    checkWinCombo(2, 5, 8) ||
+    checkWinCombo(3, 6, 9) ||
+    checkWinCombo(1, 5, 9) ||
+    checkWinCombo(3, 5, 7)) {
+      winner = player;
+      $('.box').off("click");
+      $('.messages').text('Congrats! ' + player + ' wins!');
+      score();
+    } else {
+      return false;
+    }
+};
 
-function checkRow(a, b, c, move) {
-  var winner = false;
-  if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
-    winner = true;
-  } //check to see if squares have 3 in a row for checkForWinner
-  return result;
-}
-//reference the s1-9 boxes
-function getBox(number) {
-  return document.getElementById("s" + number).textContent;
-}
+let switchPlayer = function() {
+  if (player === 'x') {
+    player = 'o';
+  } else {
+    player = 'x';
+  }
+};
 
-function clearBox(number) {
-  document.getElementById("s" + number).textContent = "";
-}
+let move = function() {
+  $('.box').on('click', function() {
+      if ($(this).text() !== '') {
+        $('.messages').text('Select another box!');
+      } else if (player === 'x') {
+        $(this).text('x');
+        $(this).last().addClass('disable');
+        console.log ('click');
+      } else {
+        $(this).text('o');
+        $(this).last().addClass('disable');
+      }
+      checkWin();
+      switchPlayer();
+  });
+};
 
 $(document).ready(() => {
   console.log('It works.');
+  move ();
 });
