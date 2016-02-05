c06be105f489f763172f6e569e7087e7bc6dc723
msyao-github <msyaois@gmail.com>
2016-02-04 21:39:38 -0500

    Updated index JS late Thur

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,20 +6,21 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 //^this is not defined!!! needs to be defined!
-
+//the api authentication
+require('./api-auth');
 // load sass manifest
 require('../styles/index.scss');
-// let playerImg = [<img src="https://i.imgsafe.org/9d441eb.gif" height="50"/>,<img src="https://i.imgsafe.org/74d6152.gif" height="50"/>];
+
 let winner;
 let player = 'X';
 let xWins = 0;
 let oWins = 0;
 let ties = 0;
 
-
+let gameStatus = 'active';
 $("button").on('click', function() {
   $('.box').text('');
-  $('.box').removeClass('disable');
+  gameStatus = 'active';
 });
 
 
@@ -58,7 +59,7 @@ let checkWin = function() {
     checkWinCombo(1, 5, 9) ||
     checkWinCombo(3, 5, 7)) {
       winner = player;
-      $('.box').off("click");
+      gameStatus = 'inactive';
       $('.messages').text('Congrats! Player ' + player + ' Wins!');
       score();
     } else if ($('.box').text().length === 9 ){
@@ -80,21 +81,22 @@ let switchPlayer = function() {
 };
 
 
-
 let move = function() {
   $('.box').on('click', function() {
-      if ($(this).text() !== '') {
-        $('.messages').text('Select another box!');
-      } else if (player === 'X') {
-        $(this).text('X');
-        $(this).last().addClass('disable');
-        console.log ('click');
-      } else {
-        $(this).text('O');
-        $(this).last().addClass('disable');
-      }
-      checkWin();
-      switchPlayer();
+      if (gameStatus ==='active'){
+        if ($(this).text() !== '') {
+          $('.messages').text('Go To Another Box!');
+        } else if (player === 'X') {
+          $(this).text('X');
+          // $(this).last().addClass('disable');
+          console.log ('click');
+        } else {
+          $(this).text('O');
+          // $(this).last().addClass('disable');
+        }
+        checkWin();
+        switchPlayer();
+    }
   });
 };
 
